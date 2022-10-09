bdae48b49f723dc1fd72ff03f6ba1e6fd3a0fac0
Ricardo Solano Fadul <risolfa@gmail.com>
2022-10-09 16:49:37 -0500

    Al final del ejercicio con problemas en @media

--- Split landing page/js/script.js
+++ Split landing page/js/script.js
@@ -3,18 +3,22 @@ const right = document.querySelector('.right');
 const container = document.querySelector('.container');
 
 left.addEventListener('mouseenter', ()=> {
+    console.log('Enter mouse to the left')
   container.classList.remove('hover-right');
   container.classList.add('hover-left');
 });
-/*left.addEventlistener('mouseleave', ()=>{
+right.addEventListener('mouseenter', ()=> {
+    console.log('Enter mouse to the right')
+  container.classList.remove('hover-left');
+  container.classList.add('hover-right')
+});
+left.addEventListener('mouseleave', ()=>{
+    console.log('Leaving the left side')
   container.classList.remove('hover-left');
   console.log('leaving left side');
-});*/
-right.addEventListener('mouseenter', ()=> {
-  container.classList.remove('hover-left');
-  container.classlist.add('hover-right')
 });
-/*right.addEventlistener('mouseleave', ()=> {
+right.addEventListener('mouseleave', ()=> {
+    console.log('Leaving the right side')
   container.classList.remove('hover-right');
   console.log('leaving right side');
-});*/
+});
